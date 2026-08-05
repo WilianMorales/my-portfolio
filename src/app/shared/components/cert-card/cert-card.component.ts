becb271d5b1508e31210e40c2ef1c0c1d6c73cea
Wilian Morales <willian.moralesch@gmail.com>
2026-08-05 13:45:55 -0500

    refactor(shared): modernizar cert-card/project-card y tipar con certificado.interface

--- src/app/shared/components/cert-card/cert-card.component.ts
+++ src/app/shared/components/cert-card/cert-card.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Certificado } from '../../interfaces/certificado.interface';
 
 @Component({
   selector: 'app-cert-card',
   templateUrl: './cert-card.component.html',
-  styleUrls: ['./cert-card.component.css']
+  styleUrls: ['./cert-card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CertCardComponent {
-  @Input() certificado: any;
-  @Input() viewCredText: string = '';
+  readonly certificado = input.required<Certificado>();
+  readonly viewCredText = input('');
 }
---
+++ src/app/shared/interfaces/certificado.interface.ts
@@ -0,0 +1,9 @@
+export interface Certificado {
+  tipo: string;
+  titulo: string;
+  lugar: string;
+  fecha: string;
+  credencial: string;
+  url: string;
+  view: string;
+}
