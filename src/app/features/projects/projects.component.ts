06cf0e08edc8a957db5a4cc53fbc5769651e00ea
Wilian Morales <willian.moralesch@gmail.com>
2026-08-06 18:46:30 -0500

    style(ui): mejoras en diseño visual del portafolio

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Project } from '@app/shared/models/project.interface';
 import { HttpClient } from '@angular/common/http';
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { faBriefcase, faTriangleExclamation, faSpinner, faFolderOpen, faRotateRight } from '@fortawesome/free-solid-svg-icons';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { ProjectCardComponent } from '@app/shared/components/project-card/project-card.component';
 import { TranslatePipe } from '@ngx-translate/core';
+import { catchError, finalize, of } from 'rxjs';
 
 @Component({
   selector: 'app-projects',
@@ -16,11 +17,37 @@ import { TranslatePipe } from '@ngx-translate/core';
 })
 export class ProjectsComponent {
   private readonly http = inject(HttpClient);
+  private readonly destroyRef = inject(DestroyRef);
 
   readonly iconPortfolio = faBriefcase;
+  readonly iconError = faTriangleExclamation;
+  readonly iconSpinner = faSpinner;
+  readonly iconEmpty = faFolderOpen;
+  readonly iconRetry = faRotateRight;
 
-  readonly projects = toSignal(
-    this.http.get<Project[]>('assets/data/projects.json'),
-    { initialValue: [] as Project[] }
-  );
+  readonly projects = signal<Project[]>([]);
+  readonly isLoading = signal(true);
+  readonly hasError = signal(false);
+
+  constructor() {
+    this.loadProjects();
+  }
+
+  retry(): void {
+    this.loadProjects();
+  }
+
+  private loadProjects(): void {
+    this.isLoading.set(true);
+    this.hasError.set(false);
+
+    this.http.get<Project[]>('assets/data/projects.json').pipe(
+      catchError(() => {
+        this.hasError.set(true);
+        return of([] as Project[]);
+      }),
+      finalize(() => this.isLoading.set(false)),
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe(data => this.projects.set(data));
+  }
 }
